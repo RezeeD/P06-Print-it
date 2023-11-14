6a75441337d37e5397d6b557e1ae689379907de7
RezeeD <142891510+RezeeD@users.noreply.github.com>
2023-11-14 16:11:33 +0100

    Correction du code

--- assets/script.js
+++ assets/script.js
@@ -19,33 +19,128 @@ const slides = [
 
 //Etape 2 Ajouter des eventListener
 
-let listImgFleche =  document.querySelectorAll(".arrow img")
-	for (let i = 0; i < listImgFleche.length; i++){
-		let flecheActuelle = listImgFleche[i]
+let allArrow = document.querySelectorAll(".arrow img")
 
-		flecheActuelle.addEventListener("click", (event) => {
-			maFleche = event.target
-			console.log(`J'ai cliqué sur la fleche : ${maFleche.classList}`)
-		} )
+	for (i = 0 ; i < 2 ; i++) {
+		allArrow[i].addEventListener("click", (event) => {
+			myArrow=event.target
+			console.log(`J'ai cliqué sur : ${myArrow.className}`)
+		})
+
 	}
-
 
 // Etape 3 ajouter des bullet point au slider
 
-let compteurImg = slides.length
 
-let index = 0
-	while (index < compteurImg) {
+
+	for (let index = 0 ; index < 4; index++ ) {
 		let bulletPoint = document.createElement("div")
 		let bulletPointParent = document.querySelector(".dots")
-		bulletPointParent.appendChild(bulletPoint)
-		bulletPoint.classList.add("dot")
-		index++
-	}
+	 bulletPointParent.appendChild(bulletPoint)
+	 bulletPoint.classList.add("dot")
+ 	}
 
-//Etape 4 modifier le slide au clic sur le bouton
+let allBulletPoints = document.querySelectorAll(".dots .dot")
+
+let bulletPointActiveOnly1 = allBulletPoints[0]
+
+bulletPointActiveOnly1.classList.add("dot_selected")
+
+
+//Etape 4 : changer les images au clic sur les flèches et actualiser le bulletpoint actif
+
+let flecheActiveDroite = document.querySelector(".arrow_right")
+
+	flecheActiveDroite.addEventListener("click", () =>{
+
+
+		    let bannerImgActive = document.querySelector(".banner-img")
+			let indexImgActive = 0
+
+			switch (indexImgActive){
+
+				case 0:
+					bannerImgActive.setAttribute("src", "./assets/images/slideshow/slide1.jpg")
+					console.log(indexImgActive)
+
+				case 1:
+
+					bannerImgActive.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
+					console.log(indexImgActive)
+
+				case 2:
+					bannerImgActive.setAttribute("src", "./assets/images/slideshow/slide3.jpg")
+					console.log(indexImgActive)
+
+				case 3:
+
+					bannerImgActive.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
+					console.log(indexImgActive)
+		   }
+
+		})
 
 
 
+
+
+
+
+
+
+
+
+//flecheActiveDroite.addEventListener("click", () => {
+
+	//bannerImg = document.querySelector(".banner-img")
+
+	//bannerImg.setAttribute("src", "./assets/images/slideshow/slide1.jpg")
+
+//})
+
+//		flecheActiveDroite.addEventListener("click", () => {
+
+		//	bannerImg = document.querySelector(".banner-img")
+
+		//	bannerImg.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
+
+		//})
+
+//flecheActiveDroite.addEventListener("click", () => {
+
+//	bannerImg = document.querySelector(".banner-img")
+
+//	bannerImg.setAttribute("src", "./assets/images/slideshow/slide3.jpg")
+
+//})
+
+//flecheActiveDroite.addEventListener("click", () => {
+
+//	bannerImg = document.querySelector(".banner-img")
+
+//	bannerImg.setAttribute("src", "./assets/images/slideshow/slide4.png")
+
+//})
+
+
+
+
+//let bulletPointActiveOnly2 = allBulletPoints[1]
+
+//bulletPointActiveOnly1.classList.remove("dot_selected")
+//bulletPointActiveOnly2.classList.add("dot_selected")
+
+//let bulletPointActiveOnly3 = allBulletPoints[2]
+
+//bulletPointActiveOnly2.classList.remove("dot_selected")
+//bulletPointActiveOnly3.classList.add("dot_selected")
+
+//let bulletPointActiveOnly4 = allBulletPoints[3]
+
+//bulletPointActiveOnly3.classList.remove("dot_selected")
+//bulletPointActiveOnly4.classList.add("dot_selected")
+
+
+//Etape 4 modifier le slider au clic sur le bouton
 
 
