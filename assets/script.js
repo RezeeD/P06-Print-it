0b7a8a7daa5227a6b36e3fce93ade39f1247730d
RezeeD <142891510+RezeeD@users.noreply.github.com>
2023-11-22 17:13:34 +0100

    Mise en place des bulletspoints Mise en place du défilement infini

--- assets/script.js
+++ assets/script.js
@@ -1,146 +1,90 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
 //Etape 2 Ajouter des eventListener
 
-let allArrow = document.querySelectorAll(".arrow img")
+let allArrow = document.querySelectorAll(".arrow img");
 
-	for (i = 0 ; i < 2 ; i++) {
-		allArrow[i].addEventListener("click", (event) => {
-			myArrow=event.target
-			console.log(`J'ai cliqué sur : ${myArrow.className}`)
-		})
-
-	}
+for (i = 0; i < 2; i++) {
+  allArrow[i].addEventListener("click", (event) => {
+    myArrow = event.target;
+    console.log(`J'ai cliqué sur : ${myArrow.className}`);
+  });
+}
 
 // Etape 3 ajouter des bullet point au slider
 
+for (let index = 0; index < 4; index++) {
+  let bulletPoint = document.createElement("div");
+  let bulletPointParent = document.querySelector(".dots");
+  bulletPointParent.appendChild(bulletPoint);
+  bulletPoint.classList.add("dot");
+}
 
+let allBulletPoints = document.querySelectorAll(".dots .dot");
 
-	for (let index = 0 ; index < 4; index++ ) {
-		let bulletPoint = document.createElement("div")
-		let bulletPointParent = document.querySelector(".dots")
-	 bulletPointParent.appendChild(bulletPoint)
-	 bulletPoint.classList.add("dot")
- 	}
-
-let allBulletPoints = document.querySelectorAll(".dots .dot")
-
-let bulletPointActiveOnly1 = allBulletPoints[0]
-
-bulletPointActiveOnly1.classList.add("dot_selected")
-
+allBulletPoints[0].classList.add("dot_selected");
 
 //Etape 4 : changer les images au clic sur les flèches et actualiser le bulletpoint actif
 
-let flecheActiveDroite = document.querySelector(".arrow_right")
+let indexSlide = 0;
+let slideArrow = document.querySelectorAll(".arrow img");
+let slideImg = document.querySelector(".banner-img");
+let slideMsg = document.querySelector("#banner p");
 
-	flecheActiveDroite.addEventListener("click", () =>{
+slideArrow[1].addEventListener("click", () => {
+  console.log(indexSlide);
 
+  indexSlide++;
 
-		    let bannerImgActive = document.querySelector(".banner-img")
-			let indexImgActive = 0
+  if (indexSlide < 4) {
+    allBulletPoints[indexSlide - 1].classList.remove("dot_selected");
+    allBulletPoints[indexSlide].classList.add("dot_selected");
+    slideImg.src = `./assets/images/slideshow/${slides[indexSlide].image}`;
+    slideMsg.innerHTML = `<p>${slides[indexSlide].tagLine}</p>`;
+  } else if (indexSlide > 3) {
+    indexSlide = 0;
+    allBulletPoints[3].classList.remove("dot_selected");
+    allBulletPoints[indexSlide].classList.add("dot_selected");
+    slideImg.src = `./assets/images/slideshow/${slides[indexSlide].image}`;
+    slideMsg.innerHTML = `<p>${slides[indexSlide].tagLine}</p>`;
+  }
+});
 
-			switch (indexImgActive){
+slideArrow[0].addEventListener("click", () => {
+  console.log(indexSlide);
 
-				case 0:
-					bannerImgActive.setAttribute("src", "./assets/images/slideshow/slide1.jpg")
-					console.log(indexImgActive)
+  indexSlide--;
 
-				case 1:
+  if (indexSlide < 0) {
+    indexSlide = 3;
 
-					bannerImgActive.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
-					console.log(indexImgActive)
-
-				case 2:
-					bannerImgActive.setAttribute("src", "./assets/images/slideshow/slide3.jpg")
-					console.log(indexImgActive)
-
-				case 3:
-
-					bannerImgActive.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
-					console.log(indexImgActive)
-		   }
-
-		})
-
-
-
-
-
-
-
-
-
-
-
-//flecheActiveDroite.addEventListener("click", () => {
-
-	//bannerImg = document.querySelector(".banner-img")
-
-	//bannerImg.setAttribute("src", "./assets/images/slideshow/slide1.jpg")
-
-//})
-
-//		flecheActiveDroite.addEventListener("click", () => {
-
-		//	bannerImg = document.querySelector(".banner-img")
-
-		//	bannerImg.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
-
-		//})
-
-//flecheActiveDroite.addEventListener("click", () => {
-
-//	bannerImg = document.querySelector(".banner-img")
-
-//	bannerImg.setAttribute("src", "./assets/images/slideshow/slide3.jpg")
-
-//})
-
-//flecheActiveDroite.addEventListener("click", () => {
-
-//	bannerImg = document.querySelector(".banner-img")
-
-//	bannerImg.setAttribute("src", "./assets/images/slideshow/slide4.png")
-
-//})
-
-
-
-
-//let bulletPointActiveOnly2 = allBulletPoints[1]
-
-//bulletPointActiveOnly1.classList.remove("dot_selected")
-//bulletPointActiveOnly2.classList.add("dot_selected")
-
-//let bulletPointActiveOnly3 = allBulletPoints[2]
-
-//bulletPointActiveOnly2.classList.remove("dot_selected")
-//bulletPointActiveOnly3.classList.add("dot_selected")
-
-//let bulletPointActiveOnly4 = allBulletPoints[3]
-
-//bulletPointActiveOnly3.classList.remove("dot_selected")
-//bulletPointActiveOnly4.classList.add("dot_selected")
-
-
-//Etape 4 modifier le slider au clic sur le bouton
-
+    allBulletPoints[0].classList.remove("dot_selected");
+    allBulletPoints[indexSlide].classList.add("dot_selected");
+    slideImg.src = `./assets/images/slideshow/${slides[indexSlide].image}`;
+    slideMsg.innerHTML = `<p>${slides[indexSlide].tagLine}</p>`;
+  } else if (indexSlide < 4) {
+    allBulletPoints[indexSlide + 1].classList.remove("dot_selected");
+    allBulletPoints[indexSlide].classList.add("dot_selected");
+    slideImg.src = `./assets/images/slideshow/${slides[indexSlide].image}`;
+    slideMsg.innerHTML = `<p>${slides[indexSlide].tagLine}</p>`;
+  }
+});
 
